6589eb51bfc55200426c7feda57f7d74cae572b3
Kaushal Dalvi <kaushaldalvi@gmail.com>
2022-12-01 22:53:27 -0500

    day 01

---
+++ day-01.py
@@ -0,0 +1,34 @@
+def read_file(param):
+    with open(param, 'r') as f:
+        return f.read()
+
+
+if __name__ == '__main__':
+    lines = read_file('01-input.txt')
+
+    current_elf_calories = 0
+    top_1_calories = 0
+    top_2_calories = 0
+    top_3_calories = 0
+
+    for line in lines.splitlines():
+        if line == '':
+            if current_elf_calories >= top_3_calories:
+                if current_elf_calories >= top_2_calories:
+                    if current_elf_calories >= top_1_calories:
+                        top_3_calories = top_2_calories
+                        top_2_calories = top_1_calories
+                        top_1_calories = current_elf_calories
+                    else:
+                        top_3_calories = top_2_calories
+                        top_2_calories = current_elf_calories
+                else:
+                    top_3_calories = current_elf_calories
+
+            current_elf_calories = 0
+
+        else:
+            current_elf_calories = current_elf_calories + int(line)
+
+    print("Top 3 values of heap : " + str(top_1_calories) + " " + str(top_2_calories) + " " + str(top_3_calories))
+    print("Sum of top 3 values of heap : " + str(top_1_calories + top_2_calories + top_3_calories))
